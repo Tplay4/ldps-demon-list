d109f6ece0377ce69bb523e036e2fc9f5c9ad624
Tplay4 <116071065+Tplay4@users.noreply.github.com>
2025-08-08 22:22:37 +0700

    Create index.css

---
+++ index.css
@@ -0,0 +1,45 @@
+<?php
+
+// CSS styles
+
+$style = "";
+
+echo($style);
+
+// List
+
+$json = file_get_contents("./list.json");
+$list = json_decode($json, true);
+
+$index = 0;
+$param = 0;
+
+foreach($list as $level) {
+	$index = $index + 1;
+	echo("<h1>#" . $index .  "</h1>");
+	foreach($level as $key) {
+		switch($param) {
+			case 0:
+			echo("<h2>Name:" . $key . "<br></h2>");	
+			$param = $param + 1;
+			break;
+			
+			case 1:
+			echo("<h2>ID:" . $key . "<br></h2>");
+			$param = $param + 1;
+			break;
+			
+			case 2:
+			echo("<h2>Creator:" . $key . "<br></h2>");
+			$param = $param + 1;
+			break;
+			
+			case 3:
+			echo("<h2>Verifier:" . $key . "<br></h2>");
+			$param = 0;
+			break;
+		}
+	}
+}
+
+?>
